61128af65dc7b298a39a2d6e40e69e777adfd958
Sakthipraba Manjunath <u499790@wns.com>
2025-06-09 10:35:53 +0530

    library

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Concurrent;
+using System.Net;
+using System.Reflection;
+using LibraryManagement;
+
+//Object for book class
+Book b1 = new Book { Title = "Think like a Monk", Author = "Jay Shetty" };
+
+b1.DisplayDetails();
---
+++ book.cs
@@ -0,0 +1,21 @@
+namespace LibraryManagement;
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+
+    public Book(string title, string author, string isbn)
+    {
+        Title = title;
+        Author = author;
+        ISBN = isbn;
+    }
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Title : {Title}");
+        Console.WriteLine($"Author : {Author}");
+        Console.WriteLine($"ISBN : {ISBN}");
+    }
+
+}
